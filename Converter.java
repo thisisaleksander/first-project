dc00d0071e2a4c6c3d30e58609891848580e44fa
thisisaleksander <91897315+thisisaleksander@users.noreply.github.com>
2022-08-30 10:48:34 +0300

    Add files via upload

--- Converter.java
+++ Converter.java
@@ -1,3 +1,13 @@
-Public static void Converter() {
+public class Converter {
+    public double convertToKM(int sumOfSteps) {
+        double totalDistance = 0.0;
+        totalDistance = sumOfSteps * 0.75 / 1000;
+        return totalDistance;
+    }
 
+    public double convertToCalories(int sumOfSteps) {
+        double totalCalories = 0.0;
+        totalCalories = sumOfSteps * 50 / 1000;
+        return totalCalories;
+    }
 }
